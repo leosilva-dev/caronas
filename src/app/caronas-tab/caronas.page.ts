ee3eda0d3f5e6b3e09ecadebe4929a96f31c0104
Leonardo Silva <silvaleonardo.ti@gmail.com>
2023-08-28 19:51:09 -0300

    feat: removendo tabs genericas e incluindo tabs para secoes do app

---
+++ src/app/caronas-tab/caronas.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'caronas',
+  templateUrl: 'caronas.page.html',
+  styleUrls: ['caronas.page.scss']
+})
+export class CaronasPage {
+
+  constructor() {}
+
+}
