954403480c5335371bbd66b63aaab7a98202ddf4
Leonardo Silva <silvaleonardo.ti@gmail.com>
2023-09-07 09:41:35 -0300

    feat: add perfil form

--- src/app/perfil-tab/perfil.page.ts
+++ src/app/perfil-tab/perfil.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CaronasService } from '../caronas.service';
+
 
 @Component({
   selector: 'perfil',
@@ -7,8 +9,30 @@ import { Router } from '@angular/router';
   styleUrls: ['perfil.page.scss']
 })
 export class PerfilPage {
+  nome: string = '';
+  cidadeSaida: string = '';
+  setor: string = '';
+  telefone: string = '';
+  numeroVagas: number = 1;
+  ofereceCarona: boolean = false;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private caronasService: CaronasService) {}
+
+  salvar(){
+    this.caronasService.createCarona({
+      id: 0,
+      name: this.nome,
+      cidade:this.cidadeSaida,
+      ofereceCarona: this.ofereceCarona,
+      phone: this.telefone,
+      setor: this.setor,
+      vagasRestantes: this.numeroVagas
+    })
+  }
+
+  handleToggleCarona(){
+    this.ofereceCarona = !this.ofereceCarona
+  }
 
   sair(){
     this.router.navigate(['/']);
